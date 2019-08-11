f0cf9b5a1214a3bbaa56e1bdd18def5f7ea797dd
定逺 <dingyuan.yxd@alibaba-inc.com>
2019-08-11 09:07:35 +0800

    aa

--- new_fc_mining.py
+++ new_fc_mining.py
@@ -511,11 +511,11 @@ def buy_main_body(mutex2,api,bidirection,partition,_money,_coin,min_size,money_h
 
 
                 money, coin, freez_money, freez_coin = api.get_available_balance(_money, _coin, trade_type)
-                trade_amount = freez_money + (coin + freez_coin) * buy1
+
                 current_value = (money + freez_money) + (coin + freez_coin) * buy1
                 print("trade_pair:",market,"value loss:",init_value-current_value)
                 print("time spent:",time.time()-begin_time)
-                if trade_amount<money_have and small_trade and init_value-current_value<tolerant_loss:# and init_value-current_value>-1*tolerant_loss:
+                if trade_type=="margin" and small_trade and init_value-current_value<tolerant_loss:# and init_value-current_value>-1*tolerant_loss:
                     small_step=3*min_size
                     if money/buy1>small_step:
                         id = api.take_order(market, "buy", buy1, small_step, coin_place, trade_type)
